db5435d724cea1024bf5fed391552fe9719c5232
Sungho <jy390810@gmail.com>
2023-02-07 23:41:04 +0900

    add write_remidx function

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import yaml
+import os
 
 def save_yaml(path, obj):
     with open(path, 'w') as f:
@@ -7,3 +8,10 @@ def save_yaml(path, obj):
 def load_yaml(path):
     with open(path, 'r') as f:
         return yaml.load(f, Loader=yaml.FullLoader)
+
+def write_remidx(M, K, ppath, name) :
+    outfile = os.path.join(ppath, name, 'remidx_' + name + '.txt')
+    f = open(outfile, 'w')
+    s = ["%d\t%d\n" % (i,j) for (i,j) in zip(M,K)]
+    f.writelines(s)
+    f.close()
